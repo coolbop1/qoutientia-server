de39704bd7ed2fa3c1a02472524b6dea5527ff06
Bidemi <bidemi@seamlesshr.com>
2021-03-12 15:49:28 +0100

    reduce token

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -180,9 +180,6 @@ class AuthController extends Controller
     protected function createNewToken($token){
         return response()->json([
             'access_token' => $token,
-            'token_type' => 'bearer',
-            'expires_in' => auth()->factory()->getTTL() * 60,
-            'user' => auth()->user()
         ]);
     }
 
